883ffdccec640aa6592ac8b88152ecf269adf53c
Nathan Matteson <matteson@obstructures.org>
2015-08-09 13:21:16 -0500

    Load csv

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
 (function() {
 
+  //Load data from CSV file.
+  d3.csv('../csv/City Data Spreadsheet - Tallies.csv', function(data) {
 
+    // The csv call is asynchronous, so all processing or drawing happens
+    // with this data happens 'inside' the call.
+
+    console.log(data);
+
+  });
 
 })();
