bacb12ecca34dbae4e7368c9be5b52b77d1c3aa1
Nathan Matteson <matteson@obstructures.org>
2015-08-09 20:37:03 -0500

    Draw basic chart

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,37 @@
 (function() {
 
-  var datetime_format = d3.time.format('%-m/%d/%Y%H:%M')
+  // A load of new variables for our chart. Beginning with basic layout:
+  // height, width, and margins.
+  var margin = {top: 10, right: 10, bottom: 10, left:10}
+    , w = 600
+    , h = 400
+    , width = w - margin.right - margin.left
+    , height = h - margin.top - margin.bottom
+    // x and y scales.
+    , x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1)
+    , y = d3.scale.linear()
+      .range([height, 0])
+    // The <svg> element, and a child <g> appended
+    , svg = d3.select('body').append('svg')
+      .attr('width', w)
+      .attr('height', h)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    // functions to draw the x- and y-axes
+    , x_axis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom')
+    , y_axis = d3.svg.axis()
+      .scale(y)
+      .orient('left')
+    // 'Layers' (<g> elements) for the axes
+    , x_axis_layer = svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + height + ')')
+    , y_axis_layer = svg.append('g')
+      .attr('class', 'y axis')
+    , datetime_format = d3.time.format('%-m/%d/%Y%H:%M')
     , data_by_area
   ;
 
@@ -34,11 +65,6 @@
 
     });
 
-    // d3 nest() can be confusing. The data isn't really in a usable form,
-    // with each datum in its own row. We need to 'group' it somehow. We'll
-    // group it by Date (since these dates ultimately represent areas of
-    // the city). Then we need to construct an object that holds the sum
-    // of several columns in the CSV.
     data_by_area = d3.nest()
       .key(function(d) { return d.Date; })
       .rollup(function(leaves) {
@@ -50,7 +76,34 @@
       })
       .entries(data);
 
-    console.log(data_by_area);
+    // Set the domains of x and y
+    // The domain of x will be an array of all the dates on which
+    // data was recorded --- effectively neighborhoods. This is stored
+    // as the key of the objects in the nested data.
+    x.domain(data_by_area.map(function(d) { return d.key; }));
+
+    // The domain of y is from zero the maximum amount of ForSale signs
+    // (or whatever else we want to chart).
+    y.domain([0, d3.max(data_by_area, function(d) { return d.values.ForSale; })]);
+
+    // This is really the brunt of d3 --- this is where data gets bound to
+    // elements. We selectAll() several objects *that don't yet exist*.
+    // Then data() assigns values to those objects (it's argument must be an
+    // array). enter() finally creates the objects. And append() creates
+    // and element in our HTML for each object.
+    svg.selectAll('.bar')
+      .data(data_by_area)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d) { return x(d.key); })
+      .attr('y', function(d) { return y(d.values.ForSale); })
+      .attr('height', function(d) { return height - y(d.values.ForSale); })
+      .attr('width', x.rangeBand());
+
+    // Draw the axes
+    x_axis_layer.call(x_axis);
+    y_axis_layer.call(y_axis);
 
   });
 
